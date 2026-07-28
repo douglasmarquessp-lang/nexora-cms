acfda06ff50e64bc01fb87ad520138175158e01f
douglasmarquessp-lang <douglasmarquessp@gmail.com>
2026-07-28 20:58:49 -0300

    Delete migrations/000013_add_generation_tables.down.sql

--- migrations/000013_add_generation_tables.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS generation_stats;
-DROP TABLE IF EXISTS generation_quality_gates;
-DROP TABLE IF EXISTS generation_pipeline_logs;
-DROP TABLE IF EXISTS generation_pipeline;
-DROP TABLE IF EXISTS generation_jobs;
