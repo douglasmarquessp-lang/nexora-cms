0dc20ed3839aa1244e9dfdd472e956101b990012
douglasmarquessp-lang <douglasmarquessp@gmail.com>
2026-07-28 20:56:03 -0300

    Delete migrations/000003_add_audit_log.down.sql

--- migrations/000003_add_audit_log.down.sql
+++
@@ -1,7 +0,0 @@
--- 000003_add_audit_log.down.sql
-
-DROP INDEX IF EXISTS idx_audit_log_entity;
-DROP INDEX IF EXISTS idx_audit_log_created;
-DROP INDEX IF EXISTS idx_audit_log_action;
-DROP INDEX IF EXISTS idx_audit_log_user;
-DROP TABLE IF EXISTS audit_log;
