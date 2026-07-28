f0e574202e4da63bb1be7abc7cb007d201d947e9
douglasmarquessp-lang <douglasmarquessp@gmail.com>
2026-07-28 20:58:00 -0300

    Delete migrations/000009_add_editorial.down.sql

--- migrations/000009_add_editorial.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS editorial_widgets;
-DROP TABLE IF EXISTS editorial_calendar_events;
-DROP TABLE IF EXISTS approval_requests;
-DROP TABLE IF EXISTS post_revisions;
-DROP TABLE IF EXISTS editorial_tasks;
