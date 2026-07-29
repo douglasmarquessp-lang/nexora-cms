0085f3c349a21b83ba02c9f4a199fc203ce05c69
douglasmarquessp-lang <douglasmarquessp@gmail.com>
2026-07-28 21:01:06 -0300

    Delete migrations/000014_add_autocontent_tables.down.sql

--- migrations/000014_add_autocontent_tables.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS workflow_templates CASCADE;
-DROP TABLE IF EXISTS publication_queue CASCADE;
-DROP TABLE IF EXISTS autocontent_results CASCADE;
-DROP TABLE IF EXISTS autocontent_steps CASCADE;
-DROP TABLE IF EXISTS autocontent_jobs CASCADE;
